4592ee4b48aa2d6736c73910c33a37cda63fe37a
MNMINC <157410433+MNMINC@users.noreply.github.com>
2024-03-16 02:18:49 +0000

    Get Prime

--- main.hpp
+++ main.hpp
@@ -1,8 +1,38 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int begin, int end)
+{
+    do 
+    {
+        cin >> begin >> end;
+    } while (begin >= end);
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+int getNextPrime(int begin)
+{
+    int np = begin + 1;
+    while (!IsPrime(np))
+    {
+        ++np;
+    }
+    return np;
+}
+int getPervPrime(int end)
+{
+    int np = end - 1;
+    while (!IsPrime(np))
+    {
+        --np;
+    }
+    return np;
+
+}
+
+bool IsPrime(int n) 
+{
+    for (int f = 2; f < n; ++f)
+        if (n % f == 0)
+            return false; 
+    return true;
+}
